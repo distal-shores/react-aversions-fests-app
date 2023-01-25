65abd4d9dce9a3c4fd87181448f8745084e9d16f
distal-shores <samcoll@gmail.com>
2023-01-25 10:51:44 -0800

    Add error page.

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useRouteError } from "react-router-dom";
+import { Container } from 'react-bulma-components';
+
+export default function ErrorPage() {
+    const error = useRouteError();
+    console.error(error);
+    return (
+        <Container id="error-page">
+            <h1>Oops!</h1>
+            <p>Sorry, an unexpected error has occurred.</p>
+            <p>
+                <i>{error.statusText || error.message}</i>
+            </p>
+        </Container>
+    )
+}
