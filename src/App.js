782fd1ab6c3b801777d70d485869b15cd47feb70
distal-shores <samcoll@gmail.com>
2023-01-25 11:41:46 -0800

    Login rendering and dashboard child of root.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import 'bulma/css/bulma.min.css';
 import Preferences from './components/Preferences';
+import Login from './components/Login';
 import { Container } from 'react-bulma-components';
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 function App() {
+  const [token, setToken] = useState();
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
   return (
     <Container>
       <nav>
         <ul>
           <li>
-            <Link to={`/login`}>Login</Link>
-          </li>
-          <li>
             <Link to={`/dashboard`}>Dashboard</Link>
           </li>
         </ul>
       </nav>
+      <div>
+        <Outlet />
+      </div>
     </Container>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ErrorPage from "./components/ErrorPage";
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App/>,
     errorElement: <ErrorPage />,
-  },
-  {
-    path: "dashboard",
-    element: <Dashboard />,
-    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+        errorElement: <ErrorPage />,
+      }
+    ]
   },
   {
     path: "login",
